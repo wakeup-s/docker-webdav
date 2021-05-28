d25224d86cf9e72f4ad25cc3c39c1446764abd39
Aleksei Momot <a.momot@wakeup-s.com>
2021-05-28 18:31:44 +0300

    Robots.txt, permissions fix

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -18,7 +18,7 @@ HTTPD_PREFIX="${HTTPD_PREFIX:-/usr/local/apache2}"
 [ ! -e "/var/lib/dav/data/common/db.kdbx" ] && mkdir -p "/var/lib/dav/data/common" && cp "/storage/db.kdbx" "/var/lib/dav/data/common/db.kdbx"
 [ ! -e "/var/lib/dav/data/secure/db.kdbx" ] && mkdir -p "/var/lib/dav/data/secure" && cp "/storage/db.kdbx" "/var/lib/dav/data/secure/db.kdbx"
 [ ! -e "/var/lib/dav/data/critical/db.kdbx" ] && mkdir -p "/var/lib/dav/data/critical" && cp "/storage/db.kdbx" "/var/lib/dav/data/critical/db.kdbx"
-[ ! -e "/htpasswd/user.passwd" ] && touch "/htpasswd/user.passwd" && chown app:app "/htpasswd/user.passwd" && chmod 0640 "/htpasswd/user.passwd"
+[ ! -e "/htpasswd/user.passwd" ] && touch "/htpasswd/user.passwd" && chown -R app:app "/htpasswd" && chmod 0640 "/htpasswd/user.passwd"
 [ ! -e "/var/lib/dav/DavLock" ] && touch "/var/lib/dav/DavLock"
 chown -R app:app "/var/lib/dav"
 
